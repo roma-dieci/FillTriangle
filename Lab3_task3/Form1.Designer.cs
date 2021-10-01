0712eb10791e37041e014754038358f08994ff0e
roma-dieci <romafau@gmail.com>
2021-10-01 15:56:15 +0300

    final

---
+++ Lab3_task3/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Lab3_task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(667, 12);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox2.Location = new System.Drawing.Point(750, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(562, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A color:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(562, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "B color:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox3.Location = new System.Drawing.Point(750, 61);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.DoubleClick += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(667, 61);
+            this.textBox2.MaxLength = 8;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 28);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(562, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "C color:";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox4.Location = new System.Drawing.Point(750, 111);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.DoubleClick += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox3.Location = new System.Drawing.Point(667, 111);
+            this.textBox3.MaxLength = 8;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 28);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("Futura PT Book", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(566, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Fill";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(646, 15);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "#";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(646, 64);
+            this.label5.Margin = new System.Windows.Forms.Padding(0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "#";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Futura PT Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.Location = new System.Drawing.Point(646, 114);
+            this.label6.Margin = new System.Windows.Forms.Padding(0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "#";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(566, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 42);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Random Triangle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(566, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 42);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Random Colors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab3_task3/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_task3
+{
+    public partial class Form1 : Form
+    {
+        PointF[] tr_points = new PointF[3];
+
+        
+        Point A = new Point(100, 50);
+        Point B = new Point (200, 300);
+        Point C = new Point(300, 40);
+
+        Color cA;
+        Color cB;
+        Color cC;
+
+        Random rnd = new Random();
+
+        public class Triangle
+        {
+            public Point A;
+            public Point B;
+            public Point C;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            tr_points[0] = A;
+            tr_points[1] = B;
+            tr_points[2] = C;
+        }
+        
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 1);
+            e.Graphics.DrawPolygon(pen, tr_points);
+        }
+
+        public Color HEXtoRGB(string hexColor)
+        {
+            int a = 255;
+            if (hexColor.Length == 8)
+            {
+                string alpha = hexColor.Substring(6, 2);
+                a = Convert.ToInt32(alpha, 16);
+            }
+            int r = Convert.ToInt32(hexColor.Substring(0, 2), 16);
+            int g = Convert.ToInt32(hexColor.Substring(2, 2), 16);
+            int b = Convert.ToInt32(hexColor.Substring(4, 2), 16);
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fill();
+        }
+
+        public void Fill()
+        {
+            Triangle abc = new Triangle();
+            abc.A = A;
+            abc.B = B;
+            abc.C = C;
+            cA = HEXtoRGB(textBox1.Text);
+            cB = HEXtoRGB(textBox2.Text);
+            cC = HEXtoRGB(textBox3.Text);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            int minX = Math.Min(A.X, Math.Min(B.X, C.X));
+            int maxX = Math.Max(A.X, Math.Max(B.X, C.X));
+            int minY = Math.Min(A.Y, Math.Min(B.Y, C.Y));
+            int maxY = Math.Max(A.Y, Math.Max(B.Y, C.Y));
+            for (int i = minX; i <= maxX; i++)
+                for (int j = minY; j <= maxY; j++)
+                {
+                    if (inTriangle(new Point(i, j), abc))
+                    {
+                        double x = i;
+                        double y = j;
+                        double L1 = ((B.Y - C.Y) * (x - C.X) + (C.X - B.X) * (y - C.Y)) / ((B.Y - C.Y) * (A.X - C.X) + (C.X - B.X) * (A.Y - C.Y));
+                        double L2 = ((C.Y - A.Y) * (x - C.X) + (A.X - C.X) * (y - C.Y)) / ((B.Y - C.Y) * (A.X - C.X) + (C.X - B.X) * (A.Y - C.Y));
+                        double L3 = 1 - L1 - L2;
+                        if (L1 >= 0 && L2 >= 0 && L3 >= 0 && L1 <= 1 && L2 <= 1 && L3 <= 1)
+                            bmp.SetPixel(i, j, Color.FromArgb(Convert.ToInt32(L1 * cA.A + L2 * cB.A + L3 * cC.A), Convert.ToInt32(L1 * cA.R + L2 * cB.R + L3 * cC.R), Convert.ToInt32(L1 * cA.G + L2 * cB.G + L3 * cC.G), Convert.ToInt32(L1 * cA.B + L2 * cB.B + L3 * cC.B)));
+                    }
+
+                }
+
+            pictureBox1.Image = bmp;
+        }
+
+       //public Color AddColor(Color c1, Color c2)
+       //{
+       //    return Color.FromArgb((c1.A + c2.A) % 256, (c1.R + c2.R) % 256, (c1.G + c2.G) % 256, (c1.B + c2.B) % 256);
+       //}
+       //
+       //public Color SubtractColor(Color c1, Color c2)
+       //{
+       //    return Color.FromArgb( c1.A >= c2.A ? c1.A - c2.A : 0, c1.R >= c2.R ? c1.R - c2.R : 0, c1.G >= c2.G ? c1.G - c2.G : 0, c1.B >= c2.B ? c1.B - c2.B : 0);
+       //}
+       //
+       //public Color MultiplyColor(Color c, double k)
+       //{
+       //    return Color.FromArgb((int)(c.A * k), (int)(c.R * k), (int)(c.G * k), (int)(c.B * k));
+       //}
+
+        public Boolean inTriangle(Point p0, Triangle t)
+        {
+            int x0 = p0.X;
+            int x1 = t.A.X;
+            int x2 = t.B.X;
+            int x3 = t.C.X;
+            int y0 = p0.Y;
+            int y1 = t.A.Y;
+            int y2 = t.B.Y;
+            int y3 = t.C.Y;
+
+            int a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0);
+            int b = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0);
+            int c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0);
+            if ((a > 0 && b > 0 && c > 0) || (a < 0 && b < 0 && c < 0))
+                return true;
+            return false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = HEXtoRGB(textBox1.Text);
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.BackColor = HEXtoRGB(textBox2.Text);
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.BackColor = HEXtoRGB(textBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            A = new Point(rnd.Next(20, pictureBox1.Width - 20), rnd.Next(20, pictureBox1.Height - 20));
+            B = new Point(rnd.Next(20, pictureBox1.Width - 20), rnd.Next(20, pictureBox1.Height - 20));
+            C = new Point(rnd.Next(20, pictureBox1.Width - 20), rnd.Next(20, pictureBox1.Height - 20));
+            tr_points[0] = A;
+            tr_points[1] = B;
+            tr_points[2] = C;
+
+            pictureBox1.Image = null;
+            PaintEventArgs p = new PaintEventArgs(pictureBox1.CreateGraphics(), pictureBox1.Bounds);
+            pictureBox1_Paint(sender, p);
+        }
+
+        public string HexGenerator()
+        {
+            string HEX = "0123456789ABCDEF";
+            string res = "";
+            
+            for (int i = 0; i < 8; i++)
+            {
+                int rnd_val = rnd.Next(16);
+                res += HEX[rnd_val];
+                rnd_val = rnd.Next(16);
+            }
+            return res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string random_hex;
+            Color col;
+
+            random_hex = HexGenerator();
+            textBox1.Text = random_hex;
+            col = HEXtoRGB(random_hex);
+            pictureBox2.BackColor = col;
+            cA = col;
+
+            random_hex = HexGenerator();
+            textBox2.Text = random_hex;
+            col = HEXtoRGB(random_hex);
+            pictureBox3.BackColor = col;
+            cB = col;
+
+            random_hex = HexGenerator();
+            textBox3.Text = random_hex;
+            col = HEXtoRGB(random_hex);
+            pictureBox4.BackColor = col;
+            cC = col;
+
+            Fill();
+        }
+    }
+}
